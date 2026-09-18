0a082c77cbb974ed3f07ce961274f1211b0c76b5
joedrygonzalez <joedry252@gmail.com>
2026-09-18 15:27:26 +0000

    Exercici començat

---
+++ 02-RotX/src/RotX.java
@@ -0,0 +1,23 @@
+public static class RotX {
+    private static String abcedari = "aáàbcçdeéèfghiíìïjklmnñoóòpqrstuúùüvwxyz"
+    private static char[] minuscules = abcedari.toCharArray();
+    private static char[] majuscules = abcedari.toUpperCase().toCharArray();
+    private static int arrayLength = minuscules.length;
+
+    public static void main(String[] args) {
+        String[] texts = {"ABC", "XYZ", "Hola, Mr. calçot", "Perdó, per tu què és?"};
+        
+    }
+
+    public static String xifraRotX(cadena, desplaçament) {
+
+    }
+
+    public static String desxifraRotX(cadena, desplaçament) {
+
+    }
+
+    public static String forcaBrutaRotX(cadenaXifrada) {
+
+    }
+}
