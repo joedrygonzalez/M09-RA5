43931be9dcf524ee40f75909903a14a189dad9ea
joedrygonzalez <joedry252@gmail.com>
2026-09-25 17:17:20 +0200

    Iniciant l'exercici Monoalfabetic

---
+++ 03-Monoalfabetic/src/Monoalfabetic.java
@@ -0,0 +1,19 @@
+public class Monoalfabetic {
+    private static final String alfabet = "aáàbcçdeéèfghiíìïjklmnñoóòpqrstuúùüvwxyz";
+    
+    public static String main(String[] args) {
+        
+    }
+    
+    public static List<Character> permutaAlfabet(String alfabet) {
+
+    }
+
+    public static String xifraMonoAlfa(String cadena) {
+
+    }
+
+    public static String desxifraMonoAlfa(String cadena) {
+
+    }
+}
